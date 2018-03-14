68f1a583a8e1e39943ad09c8cba07855edf03990
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2018-03-14 16:55:09 -0600

    Support for Analytics helix endpoint. Support for 403 Forbidden response.

---
+++ TwitchLib.Api/Exceptions/ForbiddenResourceException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitchLib.Api.Exceptions
+{
+    /// <inheritdoc />
+    /// <summary>Exception representing a response from Twitch to attempting to access a resource a user is not allowed to.</summary>
+    public class ForbiddenResourceException : Exception
+    {
+        /// <inheritdoc />
+        /// <summary>Exception constructor</summary>
+        public ForbiddenResourceException(string data)
+            : base(data)
+        {
+        }
+    }
+}
